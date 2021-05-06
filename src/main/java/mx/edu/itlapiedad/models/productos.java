65ece8b9a73501abbf5af10bd95ee160ab83ba63
CarolinaArriaga <Carolina.arriaga.271cb84@gmail.com>
2021-05-06 00:05:52 -0500

    agregue el modelo

--- src/main/java/mx/edu/itlapiedad/models/productos.java
+++ src/main/java/mx/edu/itlapiedad/models/productos.java
@@ -4,4 +4,39 @@ public class productos {
 
 	private int id;
 	private String descripcion;
+	private float precio;
+	private  int codigo_barras;
+	private int existencia;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public int getCodigo_barras() {
+		return codigo_barras;
+	}
+	public void setCodigo_barras(int codigo_barras) {
+		this.codigo_barras = codigo_barras;
+	}
+	public int getExistencia() {
+		return existencia;
+	}
+	public void setExistencia(int existencia) {
+		this.existencia = existencia;
+	}
+	
 }
